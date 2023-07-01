d44e1caafb125bb4ef1501dfedba49660f1ba289
goyalpramod <goyalpramod1729@gmail.com>
2023-07-02 03:01:04 +0530

    add api endpoint

---
+++ api.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+from chatbot import generate_message
+
+app = FastAPI()
+
+@app.post("/get_itinerary")
+def make_ititinerary(location: str, duration: str):
+    output = generate_message(location,duration)
+    return output
+
+if __name__ == "__main__":
+    uvicorn.run("api:app", port=8080, reload=True)
